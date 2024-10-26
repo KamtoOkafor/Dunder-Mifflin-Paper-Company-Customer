321ce26f83bf1742716171cd358c44995e6a9f2c
Kamtochukwu Chukwuneme Okafor <kokafor2@umd.edu>
2024-10-26 08:43:02 -0400

    crated file structure

---
+++ customer/src/main/resources/database/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Customer (FIRST_NAME, LAST_NAME, EMAIL, STATE, PHONE_NUMBER) VALUES ('Alice', 'Smith', 'alice@example.com', 'NY', '123-456-7890');
+INSERT INTO Customer (FIRST_NAME, LAST_NAME, EMAIL, STATE, PHONE_NUMBER) VALUES ('Bob', 'Johnson', 'bob@example.com', 'CA', '987-654-3210');
+INSERT INTO Customer (FIRST_NAME, LAST_NAME, EMAIL, STATE, PHONE_NUMBER) VALUES ('Charlie', 'Brown', 'charlie@example.com', 'TX', '555-123-4567');
